734a3e9d7d2fc17730e38713775ac695f1b7e1cf
BrendanWorks <brendanworks@gmail.com>
2026-01-31 18:28:14 -0800

    Updated WordRescue.tsx

--- src/components/WordRescue.tsx
+++ src/components/WordRescue.tsx
@@ -340,7 +340,7 @@ const WordRescue = forwardRef<any, WordRescueProps>((props, ref) => {
           y: letter.y + 0.8
         }));
 
-        return updated.filter(letter => letter.y < 600);
+        return updated.filter(letter => letter.y < 615);
       });
     }, 50);
 
@@ -499,7 +499,7 @@ const WordRescue = forwardRef<any, WordRescueProps>((props, ref) => {
     console.log('Profanity word to guarantee:', profanityWord);
 
     const initialLetters = [];
-    const screenHeight = 600;
+    const screenHeight = 615;
     const maxWidth = 650;
     const topZoneHeight = screenHeight * 0.25;
     const numInitialLetters = 18;
@@ -600,7 +600,7 @@ const WordRescue = forwardRef<any, WordRescueProps>((props, ref) => {
   }
 
   return (
-    <div className="relative w-full max-w-2xl mx-auto h-screen bg-black overflow-hidden border-2 border-cyan-400/30" style={{ height: '600px' }}>
+    <div className="relative w-full max-w-2xl mx-auto h-screen bg-black overflow-hidden border-2 border-cyan-400/30" style={{ height: '615px' }}>
       {/* Game Stats */}
       <div className="absolute top-0 left-0 right-0 bg-black border-b-2 border-cyan-400/50 text-white p-2 z-10">
         <div className="flex justify-between items-center text-sm">
