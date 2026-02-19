2f57f5d5b3aa39ce74d46eb89c55890200868073
BrendanWorks <brendanworks@gmail.com>
2026-02-19 07:51:56 -0800

    Updated package-lock.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import removeConsole from 'vite-plugin-remove-console';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    react(),
+    ...(mode === 'production' ? [removeConsole({ includes: ['ts', 'tsx'] })] : []),
+  ],
+}));
